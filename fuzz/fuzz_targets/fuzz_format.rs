3a73b2df1e58e96547357a14891919a2b55a7c09
misha <mkhachaiants@gmail.com>
2025-05-04 14:38:54 +0300

    No arbitrary

--- fuzz/fuzz_targets/fuzz_format.rs
+++ fuzz/fuzz_targets/fuzz_format.rs
@@ -1,17 +1,8 @@
 #![no_main]
-use arbitrary::{Arbitrary, Unstructured};
 use libfuzzer_sys::fuzz_target;
 
-#[derive(Arbitrary)]
-pub struct FuzzInput {
-    pub datetime: String,
-    pub format: String,
-}
-
-fuzz_target!(|data: &[u8]| {
+fuzz_target!(|data: (String, String)| {
     use chrono::prelude::*;
 
-    if let Ok(input) = Unstructured::new(data).arbitrary::<FuzzInput>() {
-        let _ = DateTime::parse_from_str(&input.datetime, &input.format);
-    }
+    let _ = DateTime::parse_from_str(&data.0, &data.1);
 });
